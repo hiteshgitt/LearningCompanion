6f464f66140fa27a12f7dd4e38c360c9ec42d2cf
hitesh.lendi <hitesh.lendi@srvmedia.com>
2026-04-25 11:58:40 +0530

    chore: add docker deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: import('next').NextConfig = {
+  output: 'standalone',
 };
 
 export default nextConfig;
